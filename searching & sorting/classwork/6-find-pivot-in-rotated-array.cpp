3521b903a55973d11159d481363fe84afc060dba
Sachin-Dabgar <bosscoder1325@gmail.com>
2023-03-10 07:13:04 +0530

    new codes

---
+++ searching & sorting/classwork/6-find-pivot-in-rotated-array.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findPivot(int arr[], int size){
+    // int size = sizeof(arr)/sizeof(int);
+    // int size = 10;
+    int s = 0;
+    int e = size-1;
+    int m = s + (e-s)/2;
+    while(s<e){
+        if(m<size-1 && arr[m] > arr[m+1]){
+            return m;
+        }
+        
+        if(m>0 && arr[m-1] > arr[m]){
+            return m-1;
+        }
+        
+        if(arr[s] <= arr[m]){
+            s = m + 1;
+        }
+        else if(arr[s] > arr[m]){
+            e = m - 1;
+        }
+
+        m = s + (e - s) / 2;
+
+        
+    }
+
+    if(m){
+        return m;
+    }
+    
+    return -1;
+}
+
+int main(){
+    int arr[] = {10,2,3,4,5,6,7,8,9};
+    int size = sizeof(arr)/sizeof(int);
+
+    int element = findPivot(arr, size);
+    cout<<"Pivot element is "<<arr[element]<<endl;
+}
